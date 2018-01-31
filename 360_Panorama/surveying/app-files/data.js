582dc976d1e96a3839481151515abc7594201a94
aibhne <matthew.wilson@gmx.com>
2018-02-01 01:23:31 +1300

    360_Panorama/surveying

--- 360_Panorama/surveying/app-files/data.js
+++ 360_Panorama/surveying/app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-360_0044-sphere",
-      "name": "360_0044-sphere",
+      "id": "0-surveying",
+      "name": "Surveying",
       "levels": [
         {
           "tileSize": 256,
