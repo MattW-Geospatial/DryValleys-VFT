dbd1628177f37104102beca010d69daab901c3ad
aibhne <matthew.wilson@gmx.com>
2018-02-02 12:41:20 +1300

    360_Panorama/dry-valleys

---
+++ 360_Panorama/dry-valleys/app-files/data.js
@@ -0,0 +1,496 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-surveying",
+      "name": "Surveying",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": -0.6492624817418893,
+        "pitch": 0.2069174954334816,
+        "fov": 1.1162283937329582
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.091914238640431,
+          "pitch": -0.2292550921332719,
+          "rotation": 0,
+          "target": "1-lacroix-glacier-1"
+        },
+        {
+          "yaw": -2.3390722604731238,
+          "pitch": -0.2603457530490658,
+          "rotation": 0,
+          "target": "2-lacroix-glacier-2"
+        },
+        {
+          "yaw": 2.42094863818546,
+          "pitch": 0.07373232927437279,
+          "rotation": 0,
+          "target": "4-inside-lacroix-camp"
+        },
+        {
+          "yaw": 2.2344424438091703,
+          "pitch": 0.07357185486776885,
+          "rotation": 0,
+          "target": "3-camp-location"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.33897333564204857,
+          "pitch": 0.2725321583587039,
+          "title": "Leica Total Station",
+          "text": "Used for high precision surveying and includes a LiDAR scanner which provides a high resolution 3D point survey model of the surrounding terrain. The total station must be geolocated for geographically orientated and usable data - we used LINZ benchmarks, which often comprise of no more than a metal rod inserted into a rock, with accurately measured 3D coordinates."
+        },
+        {
+          "yaw": -1.6305666259130938,
+          "pitch": -0.27909852804453905,
+          "title": "Lacroix Glacier",
+          "text": "The lower glacier visible here is about 2 km long. It is the main source of water for the Priscu Stream.&nbsp;"
+        },
+        {
+          "yaw": -1.0622386741336385,
+          "pitch": -0.07319363957124736,
+          "title": "Moraines",
+          "text": ""
+        },
+        {
+          "yaw": 1.679307535598701,
+          "pitch": 0.0632373181962933,
+          "title": "Priscu Stream",
+          "text": "Originating at the Lacroix Glacier, it is 3.8 km long. The hyporheic zone is visible. Named after John C. Priscu, an ecologist at Montana State University.&nbsp;"
+        },
+        {
+          "yaw": 1.2773569052077995,
+          "pitch": -0.14180145559310198,
+          "title": "Sollas Glacier",
+          "text": "Meltwater forms the Bohner Stream, which flows into the Priscu Stream and on into Lake Bonney"
+        },
+        {
+          "yaw": 2.3334015075015273,
+          "pitch": 0.03512489382834438,
+          "title": "Lake Bonney",
+          "text": "An endorheic lake - at the catchment outlet of the Priscu Stream. Also fed by Taylor Glacier meltwater and several other smaller meltwater streams. Part of the McMurdo Long Term Ecological Research project.&nbsp;<br>"
+        },
+        {
+          "yaw": -2.3760303498425053,
+          "pitch": -0.3870594014013946,
+          "title": "Lacroix Glacier (upper)",
+          "text": "The upper Lacroix Glacier extends a further 5 km approx. It falls down onto the lower glacier between the Matterhorn and Scar Peak."
+        },
+        {
+          "yaw": -2.716409765949205,
+          "pitch": -0.39973508184479556,
+          "title": "Matterhorn",
+          "text": "~1700 m"
+        },
+        {
+          "yaw": -1.778150091971245,
+          "pitch": -0.5170468486114892,
+          "title": "Scar Peak",
+          "text": "~1850 m"
+        }
+      ]
+    },
+    {
+      "id": "1-lacroix-glacier-1",
+      "name": "Lacroix Glacier 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": -2.597564097421369,
+        "pitch": 0.12925725517199105,
+        "fov": 1.1162283937329582
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8077649140022558,
+          "pitch": -0.4448296063876338,
+          "rotation": 0,
+          "target": "2-lacroix-glacier-2"
+        },
+        {
+          "yaw": -2.0009605623147273,
+          "pitch": 0.2259405431691004,
+          "rotation": 12.566370614359176,
+          "target": "0-surveying"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0848911792481566,
+          "pitch": -0.1634443473946483,
+          "title": "Lacroix Glacier",
+          "text": "The ice is over 10 m high here"
+        },
+        {
+          "yaw": -1.7712513356797075,
+          "pitch": 0.09978576515325344,
+          "title": "Spiegel Pond",
+          "text": "Shallow pool on the Priscu Stream. Collects water then spills over into the lower catchment."
+        },
+        {
+          "yaw": -1.56027757530644,
+          "pitch": -0.020237613719618963,
+          "title": "Bohner Stream",
+          "text": "3 km long; flows into the Priscu Stream. Comprises of meltwater from the Sollas Glacier."
+        },
+        {
+          "yaw": -1.8279444038480257,
+          "pitch": -0.10020736452966972,
+          "title": "Ventifact Knobs",
+          "text": "Below Sollas Glacier"
+        },
+        {
+          "yaw": -2.7783700778315747,
+          "pitch": 0.14326385899019556,
+          "title": "Moraines",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-lacroix-glacier-2",
+      "name": "Lacroix Glacier 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": 1.5431883913542546,
+        "pitch": -0.03378244823403698,
+        "fov": 1.1162283937329582
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8646688106095954,
+          "pitch": 0.24741345824439875,
+          "rotation": 12.566370614359176,
+          "target": "1-lacroix-glacier-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.33942516358413144,
+          "pitch": -0.311986221295216,
+          "title": "Upper Lacroix Glacier",
+          "text": "Still about another 400 m above this location.&nbsp;"
+        },
+        {
+          "yaw": 2.840673403489731,
+          "pitch": 0.13246294404304493,
+          "title": "Meltwater channels",
+          "text": "This part of the glacier appears to be one of the main sites for meltwater"
+        },
+        {
+          "yaw": -2.402127827770437,
+          "pitch": -0.19438477517378772,
+          "title": "Sollas Glacier",
+          "text": "Meltwater forms the Bohner Stream"
+        },
+        {
+          "yaw": -1.7851056062751294,
+          "pitch": -0.1430205485312559,
+          "title": "Hughes Glacier",
+          "text": ""
+        },
+        {
+          "yaw": -1.508763061612715,
+          "pitch": -0.13749853124186195,
+          "title": "Sentinel Peak",
+          "text": "2014 m; 11.5 km SSW of this location"
+        },
+        {
+          "yaw": -2.8139634100445043,
+          "pitch": -0.21474763320789947,
+          "title": "Rahi Peak",
+          "text": "1830 m; 9 km SE of this location"
+        },
+        {
+          "yaw": 2.5567887323846596,
+          "pitch": -0.13076389452924886,
+          "title": "Nussbaum Riegel",
+          "text": "825 m; 5 km E of this location"
+        }
+      ]
+    },
+    {
+      "id": "3-camp-location",
+      "name": "Camp location",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.8175011929886313,
+        "pitch": 0.17699507005680992,
+        "fov": 1.0597484792445624
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3061946210735425,
+          "pitch": 0.05711360427041079,
+          "rotation": 0,
+          "target": "4-inside-lacroix-camp"
+        },
+        {
+          "yaw": -1.0213377260995227,
+          "pitch": -0.21430565106811628,
+          "rotation": 0,
+          "target": "2-lacroix-glacier-2"
+        },
+        {
+          "yaw": -0.8150369694725974,
+          "pitch": -0.13289080833660094,
+          "rotation": 0,
+          "target": "1-lacroix-glacier-1"
+        },
+        {
+          "yaw": -0.16268020753547674,
+          "pitch": 0.04063240998998019,
+          "rotation": 0,
+          "target": "0-surveying"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.820986692512129,
+          "pitch": 0.06954490688851678,
+          "title": "Spiegel Pond",
+          "text": "Shallow pool on the Priscu Stream. Collects water then spills over into the lower catchment."
+        },
+        {
+          "yaw": 2.2025595132714777,
+          "pitch": 0.2384582066035268,
+          "title": "Hyporheic zone",
+          "text": "Extends beneath and runs alongside the stream in the active permafrost layer, which thaws during summer; water is exchanged between the hyporheic zone and the stream. The depth to true permafrost layer is about 50 cm."
+        },
+        {
+          "yaw": 2.248207678211484,
+          "pitch": -0.04528194421145315,
+          "title": "Alluvial fan",
+          "text": ""
+        },
+        {
+          "yaw": -3.0540560918795965,
+          "pitch": -0.005223360603446636,
+          "title": "Lake Bonney",
+          "text": ""
+        },
+        {
+          "yaw": -0.3378806670718415,
+          "pitch": 0.09938803897808768,
+          "title": "Moraine",
+          "text": ""
+        },
+        {
+          "yaw": 2.9955681785710695,
+          "pitch": 0.14469462413102718,
+          "title": "Survey benchmark",
+          "text": "Used to geo-locate the survey gear so that we can collect data with a spatial reference. This mark is an iron tube in the ground, surrounded by a circle of rocks, has the code B983 and is 79.61 m above mean sea level."
+        }
+      ]
+    },
+    {
+      "id": "4-inside-lacroix-camp",
+      "name": "Inside Lacroix Camp",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": 0.10475497047292137,
+        "pitch": -0.026744438185280828,
+        "fov": 1.1162283937329582
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10592532433654256,
+          "pitch": 0.44514520183111017,
+          "rotation": 0,
+          "target": "3-camp-location"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1435974860336682,
+          "pitch": -0.4342684939550985,
+          "title": "Scott Polar tent",
+          "text": "A heavy, sturdy tent closely based on the original design used by the Scott polar expeditions"
+        },
+        {
+          "yaw": 0.8610880379573,
+          "pitch": 0.16974562386636904,
+          "title": "Matt Wilson",
+          "text": ""
+        },
+        {
+          "yaw": 0.02876568896447651,
+          "pitch": 0.2335288259134103,
+          "title": "Entrance doorway",
+          "text": "Not exactly easy to get through, but it keeps the wind out. Best positioned on the downwind side."
+        },
+        {
+          "yaw": -0.6851537551838405,
+          "pitch": 0.225805269027056,
+          "title": "Peyman Zawar-Reza",
+          "text": ""
+        },
+        {
+          "yaw": -1.1287708905939517,
+          "pitch": 0.5547054196677479,
+          "title": "Power station",
+          "text": "Always bring an extra power strip - there are a lot of batteries to charge"
+        },
+        {
+          "yaw": -0.7160205724184312,
+          "pitch": 0.8379471172185884,
+          "title": "Main Canon video camera",
+          "text": "Used for most of the videos in the virtual field trip"
+        },
+        {
+          "yaw": -0.8156266954303852,
+          "pitch": 1.181356365531311,
+          "title": "Action camera - sort of similar to a GoPro",
+          "text": "It's taped to a rock as a makeshift tripod - since the housing was mistakenly left behind. The matches just keep the tape from holding down the buttons."
+        },
+        {
+          "yaw": 0.05328222558214968,
+          "pitch": 1.0184642600920988,
+          "title": "Large capacity battery with solar panel",
+          "text": "Used to keep the time-lapse camera going, although the cold was still a problem"
+        },
+        {
+          "yaw": 1.2728633594015175,
+          "pitch": 1.220861609552614,
+          "title": "Thermal coffee pot",
+          "text": "We had a plentiful supply of fresh ground coffee"
+        },
+        {
+          "yaw": 1.4130666461576435,
+          "pitch": 0.8981984881447147,
+          "title": "Cups and bowls",
+          "text": "Kept clean using wet wipes"
+        },
+        {
+          "yaw": 2.4700472373866287,
+          "pitch": 0.7973943319147683,
+          "title": "Hand sanitiser",
+          "text": "Essential for hygiene in a (nearly) greywater-free camp"
+        },
+        {
+          "yaw": 2.9683522042156643,
+          "pitch": 0.8851283378869859,
+          "title": "Hash browns",
+          "text": "In a cheese toasty. Give it a try."
+        },
+        {
+          "yaw": 1.876136815812286,
+          "pitch": 0.23982861859779803,
+          "title": "Wet wipes",
+          "text": "The main way to stay clean... and to clean the dishes."
+        },
+        {
+          "yaw": -2.8505763331205323,
+          "pitch": 0.686278196677609,
+          "title": "Sandwich toaster",
+          "text": "It's amazing what delights can be created on them."
+        },
+        {
+          "yaw": -1.883486841819181,
+          "pitch": 0.6890606456146369,
+          "title": "Our trusty kettle",
+          "text": "Powered by a generator. It's definitely more convenient than heating water on a camping stove... and you can also shelter from the Antarctic winds while waiting for your brew."
+        }
+      ]
+    }
+  ],
+  "name": "Dry Valleys",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
